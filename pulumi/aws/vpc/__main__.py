ccebf4886762e2f4b3c54305a5a571f247eb3ab1
Elijah Zupancic <e.zupancic@f5.com>
2021-09-08 17:13:58 -0700

    feat: allow for custom config of VPC AZs
    This change allows for users to configure what availability zones they
    want to associate with the subnets on their VPC.

--- pulumi/aws/vpc/__main__.py
+++ pulumi/aws/vpc/__main__.py
@@ -5,6 +5,8 @@ from pulumi_aws import ec2, get_availability_zones
 stack_name = pulumi.get_stack()
 project_name = pulumi.get_project()
 
+config = pulumi.Config('vpc')
+
 vpc = ec2.Vpc(resource_name=f"eks-{project_name}-{stack_name}",
               cidr_block="10.100.0.0/16",
               enable_dns_support=True,
@@ -26,12 +28,21 @@ route_table = ec2.RouteTable(resource_name=f'vpc-route-table-{project_name}-{sta
                              tags={"Project": project_name,
                                    "Stack": stack_name})
 
-azs = get_availability_zones(state="available")
+# Use availability zones defined in the configuration file if available
+if config.get('azs'):
+    azs = config.get_object('azs')
+else:
+    azs = get_availability_zones(state="available").names
 
 public_subnets = []
 private_subnets = []
 
-for i, az in enumerate(azs.names):
+for i, az in enumerate(azs):
+    if not isinstance(az, str):
+        raise f'availability zone specified [{i}] is not a valid string value: [{az}]'
+    if az.strip() == "":
+        raise f'availability zone specified [{i}] is an empty string'
+
     public_subnet_addr = i
     subnet = ec2.Subnet(resource_name=f'{az}-k8s-public-{project_name}-{stack_name}',
                         availability_zone=az,
@@ -46,7 +57,7 @@ for i, az in enumerate(azs.names):
                               subnet_id=subnet.id)
     public_subnets.append(subnet)
 
-for i, az in enumerate(azs.names):
+for i, az in enumerate(azs):
     private_subnet_addr = (i + 1) * 16
     subnet = ec2.Subnet(resource_name=f"{az}-k8s-private-{project_name}-{stack_name}",
                         availability_zone=az,
