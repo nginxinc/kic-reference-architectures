f110ef2c990e200fd8b5909f3ed344349f55e96a
Elijah Zupancic <e.zupancic@f5.com>
2021-09-10 16:29:57 -0700

    chore: disable self-signed cert warning for endpoint test

--- pulumi/aws/sirius/verify.py
+++ pulumi/aws/sirius/verify.py
@@ -4,6 +4,8 @@ import requests
 import sys
 import json
 
+import urllib3
+
 stdin_json = json.load(sys.stdin)
 if 'application_url' not in stdin_json:
     raise ValueError("Missing expected key 'application_url' in STDIN json data")
@@ -14,7 +16,7 @@ payload = 'username=testuser&password=password'
 headers = {
   'Content-Type': 'application/x-www-form-urlencoded'
 }
-
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 response = requests.request("POST", url, headers=headers, data=payload, verify=False)
 response_code = response.status_code
 
