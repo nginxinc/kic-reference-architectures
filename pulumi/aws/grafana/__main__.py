595694308da541c855d2058109f57cbd07f9a05f
Jason Schmidt <j.schmidt@f5.com>
2021-10-29 08:50:45 -0600

    feat: convert grafana from helm chart to helm release

--- pulumi/aws/grafana/__main__.py
+++ pulumi/aws/grafana/__main__.py
@@ -2,23 +2,21 @@ import os
 
 import pulumi
 import pulumi_kubernetes as k8s
-import pulumi_kubernetes.helm.v3 as helm
-from pulumi_kubernetes.helm.v3 import FetchOpts
+from pulumi_kubernetes.helm.v3 import Release, ReleaseArgs, RepositoryOptsArgs
 
 from kic_util import pulumi_config
-
-
-# Removes the status field from the Helm Chart, so that it is
-# compatible with the Pulumi Chart implementation.
-def remove_status_field(obj):
-    if obj['kind'] == 'CustomResourceDefinition' and 'status' in obj:
-        del obj['status']
 
 
 def project_name_from_project_dir(dirname: str):
     script_dir = os.path.dirname(os.path.abspath(__file__))
     project_path = os.path.join(script_dir, '..', dirname)
     return pulumi_config.get_pulumi_project_name(project_path)
+
+
+def pulumi_prometheus_project_name():
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    prometheus_project_path = os.path.join(script_dir, '..', 'prometheus')
+    return pulumi_config.get_pulumi_project_name(prometheus_project_path)
 
 
 stack_name = pulumi.get_stack()
@@ -38,7 +36,6 @@ ns = k8s.core.v1.Namespace(resource_name='grafana',
                            opts=pulumi.ResourceOptions(provider=k8s_provider))
 
 config = pulumi.Config('grafana')
-
 adminuser = config.get('admin_user')
 if not adminuser:
     adminuser = 'admin'
@@ -48,56 +45,6 @@ if not adminuser:
 # same time that we fix the Anthos issues.
 adminpass = config.require('adminpass')
 
-chart_values = {
-    'persistence': {
-        'enabled': True
-    },
-    'adminUser': adminuser,
-    'adminPassword': adminpass,
-    "datasources": {
-        "datasources.yaml": {
-            "apiVersion": 1,
-            "datasources": [
-                {
-                    "name": "Prometheus",
-                    "type": "prometheus",
-                    "url": "http://prometheus-server.prometheus.svc.cluster.local:80",
-                    "access": "proxy",
-                    "isDefault": True
-                }
-            ]
-        }
-    },
-    "dashboardProviders": {
-        "dashboardproviders.yaml": {
-            "apiVersion": 1,
-            "providers": [
-                {
-                    "name": "default",
-                    "orgId": 1,
-                    "folder": "",
-                    "type": "file",
-                    "disableDeletion": False,
-                    "editable": True,
-                    "options": {
-                        "path": "/var/lib/grafana/dashboards/default"
-                    }
-                }
-            ]
-        }
-    },
-    "dashboards": {
-        "default": {
-            "local-dashboard": {
-                "url": "https://raw.githubusercontent.com/nginxinc/kubernetes-ingress/master/grafana/NGINXPlusICDashboard.json",
-                "token": "",
-                "datasoure": "Prometheus"
-            }
-        }
-    }
-}
-
-config = pulumi.Config('grafana')
 chart_name = config.get('chart_name')
 if not chart_name:
     chart_name = 'grafana'
@@ -111,16 +58,82 @@ helm_repo_url = config.get('grafana_helm_repo_url')
 if not helm_repo_url:
     helm_repo_url = 'https://grafana.github.io/helm-charts'
 
-chart_ops = helm.ChartOpts(
-    chart=chart_name,
-    namespace=ns.metadata.name,
-    repo=helm_repo_name,
-    fetch_opts=FetchOpts(repo=helm_repo_url),
-    version=chart_version,
-    values=chart_values,
-    transformations=[remove_status_field]
-)
+# Logic to extract the FQDN of prometheus
+prometheus_project_name = pulumi_prometheus_project_name()
+prometheus_stack_ref_id = f"{pulumi_user}/{prometheus_project_name}/{stack_name}"
+prometheus_stack_ref = pulumi.StackReference(prometheus_stack_ref_id)
+prometheus_hostname = prometheus_stack_ref.get_output('prometheus_hostname')
 
-grafana_chart = helm.Chart(release_name='grafana',
-                           config=chart_ops,
-                           opts=pulumi.ResourceOptions(provider=k8s_provider))
+grafana_release_args = ReleaseArgs(
+    chart=chart_name,
+    repository_opts=RepositoryOptsArgs(
+        repo=helm_repo_url
+    ),
+    version=chart_version,
+    namespace=ns.metadata.name,
+
+    # Values from Chart's parameters specified hierarchically,
+    values={
+        'persistence': {
+            'enabled': True
+        },
+        'adminUser': adminuser,
+        'adminPassword': adminpass,
+        "datasources": {
+            "datasources.yaml": {
+                "apiVersion": 1,
+                "datasources": [
+                    {
+                        "name": "Prometheus",
+                        "type": "prometheus",
+                        "url": "http://prometheus-server.prometheus.svc.cluster.local:80",
+                        "access": "proxy",
+                        "isDefault": True
+                    }
+                ]
+            }
+        },
+        "dashboardProviders": {
+            "dashboardproviders.yaml": {
+                "apiVersion": 1,
+                "providers": [
+                    {
+                        "name": "default",
+                        "orgId": 1,
+                        "folder": "",
+                        "type": "file",
+                        "disableDeletion": False,
+                        "editable": True,
+                        "options": {
+                            "path": "/var/lib/grafana/dashboards/default"
+                        }
+                    }
+                ]
+            }
+        },
+        "dashboards": {
+            "default": {
+                "local-dashboard": {
+                    "url": "https://raw.githubusercontent.com/nginxinc/kubernetes-ingress/master/grafana/NGINXPlusICDashboard.json",
+                    "token": "",
+                    "datasoure": "Prometheus"
+                }
+            }
+        }
+    },
+    # By default Release resource will wait till all created resources
+    # are available. Set this to true to skip waiting on resources being
+    # available.
+    skip_await=False,
+    # If we fail, clean up 
+    cleanup_on_fail=True,
+    # Provide a name for our release
+    name="grafana",
+    # Lint the chart before installing
+    lint=True,
+    # Force update if required
+    force_update=True)
+grafana_release = Release("grafana", args=grafana_release_args)
+
+status = grafana_release.status
+pulumi.export("Grafana Status", status)
