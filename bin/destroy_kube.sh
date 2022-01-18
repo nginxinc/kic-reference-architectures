aac7e2c3d1a9460e24aa7578206171f7cd5cca58
Jason Schmidt <j.schmidt@f5.com>
2022-01-18 15:24:02 -0700

    fix: re-enable the abort on error for destroy scripts

--- bin/destroy_kube.sh
+++ bin/destroy_kube.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#set -o errexit  # abort on nonzero exit status
+set -o errexit  # abort on nonzero exit status
 set -o nounset  # abort on unbound variable
 set -o pipefail # don't hide errors within pipes
 
