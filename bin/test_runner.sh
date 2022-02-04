e675fa42227a762667b3a63fb0e4b03c1dc490e7
Jason Schmidt <j.schmidt@f5.com>
2022-02-04 08:38:46 -0700

    chore: debugging actions

--- bin/test_runner.sh
+++ bin/test_runner.sh
@@ -3,5 +3,7 @@
 set -o errexit  # abort on nonzero exit status
 set -o pipefail # don't hide errors within pipes
 
-source /pulumi/projects/kic-reference-architectures/pulumi/python/venv/bin/activate
-/pulumi/projects/kic-reference-architectures/pulumi/python/venv/bin/python3 /pulumi/projects/kic-reference-architectures/bin/test.py
+#source /pulumi/projects/kic-reference-architectures/pulumi/python/venv/bin/activate
+source ./pulumi/python/venv/bin/activate
+#/pulumi/projects/kic-reference-architectures/pulumi/python/venv/bin/python3 /pulumi/projects/kic-reference-architectures/bin/test.py
+./pulumi/python/venv/bin/python3 ./bin/test.py
