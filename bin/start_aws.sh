4e05dd24d378730798f31075509dc3c967789243
Jason Schmidt <j.schmidt@f5.com>
2022-03-10 12:56:15 -0700

    fix: remove debugging logic from start_aws

--- bin/start_aws.sh
+++ bin/start_aws.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-set -x
-set -v
-
 set -o errexit  # abort on nonzero exit status
 set -o nounset  # abort on unbound variable
 set -o pipefail # don't hide errors within pipes
