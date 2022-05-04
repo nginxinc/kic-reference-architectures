c906c3fbca833b55fb963661f54c3ff8b7fedd0b
Jason Schmidt <j.schmidt@f5.com>
2022-05-04 11:34:01 -0600

    test: working on kubectl version issue

--- bin/setup_venv.sh
+++ bin/setup_venv.sh
@@ -238,7 +238,8 @@ fi
 # Add local kubectl to the virtual environment
 if [ ! -x "${VIRTUAL_ENV}/bin/kubectl" ]; then
   echo "Downloading kubectl into virtual environment"
-  KUBECTL_VERSION="$(${download_cmd} https://dl.k8s.io/release/stable.txt)"
+  KUBECTL_VERSION="1.23.6"
+  #KUBECTL_VERSION="$(${download_cmd} https://dl.k8s.io/release/stable.txt)"
   ${download_cmd} "https://dl.k8s.io/release/${KUBECTL_VERSION}/bin/${OS}/${ARCH}/kubectl" >"${VIRTUAL_ENV}/bin/kubectl"
   KUBECTL_CHECKSUM="$(${download_cmd} "https://dl.k8s.io/${KUBECTL_VERSION}/bin/${OS}/${ARCH}/kubectl.sha256")"
   echo "${KUBECTL_CHECKSUM}  ${VIRTUAL_ENV}/bin/kubectl" | ${sha256sum_cmd}
