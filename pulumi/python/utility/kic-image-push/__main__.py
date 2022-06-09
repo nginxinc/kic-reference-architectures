27904a626b4e145b5a5241a11bfcbefa44520c32
Elijah Zupancic <e.zupancic@f5.com>
2022-06-09 16:27:11 -0700

    feat: add differing behavior for container push per provider

--- pulumi/python/utility/kic-image-push/__main__.py
+++ pulumi/python/utility/kic-image-push/__main__.py
@@ -1,54 +1,17 @@
-import base64
+import importlib
 import os
-
 import pulumi
-from pulumi_aws import ecr
-
+from pulumi import Output
 from kic_util import pulumi_config
-from repository_push import RepositoryPush, RepositoryPushArgs, RepositoryCredentialsArgs
+from registries.base_registry import ContainerRegistry
 
-
-# Leaving to use EKS since this is tied to AWS....
-def aws_project_name_from_project_dir(dirname: str):
-    script_dir = os.path.dirname(os.path.abspath(__file__))
-    project_path = os.path.join(script_dir, '..', '..', 'infrastructure', 'aws', dirname)
-    return pulumi_config.get_pulumi_project_name(project_path)
+from repository_push import RepositoryPush, RepositoryPushArgs
 
 
 def project_name_from_project_dir(dirname: str):
     script_dir = os.path.dirname(os.path.abspath(__file__))
     project_path = os.path.join(script_dir, '..', dirname)
     return pulumi_config.get_pulumi_project_name(project_path)
-
-
-# Get login credentials for ECR, so that we can use it to store Docker images
-def get_ecr_credentials(registry_id: str):
-    credentials = ecr.get_credentials(registry_id)
-    token = credentials.authorization_token
-    decoded = str(base64.b64decode(token), 'utf-8')
-    parts = decoded.split(':', 2)
-    if len(parts) != 2:
-        raise ValueError("Unexpected format for decoded ECR authorization token")
-    username = pulumi.Output.secret(parts[0])
-    password = pulumi.Output.secret(parts[1])
-    return RepositoryCredentialsArgs(username=username, password=password)
-
-
-stack_name = pulumi.get_stack()
-project_name = pulumi.get_project()
-pulumi_user = pulumi_config.get_pulumi_user()
-
-ecr_project_name = aws_project_name_from_project_dir('ecr')
-ecr_stack_ref_id = f"{pulumi_user}/{ecr_project_name}/{stack_name}"
-ecr_stack_ref = pulumi.StackReference(ecr_stack_ref_id)
-ecr_repository_url = ecr_stack_ref.require_output('repository_url')
-ecr_registry_id = ecr_stack_ref.require_output('registry_id')
-ecr_credentials = ecr_registry_id.apply(get_ecr_credentials)
-
-kic_image_build_project_name = project_name_from_project_dir('kic-image-build')
-kic_image_build_stack_ref_id = f"{pulumi_user}/{kic_image_build_project_name}/{stack_name}"
-kick_image_build_stack_ref = pulumi.StackReference(kic_image_build_stack_ref_id)
-ingress_image = kick_image_build_stack_ref.require_output('ingress_image')
 
 
 def select_image_name(image):
@@ -77,6 +40,16 @@ def select_image_tag(image):
     return image['image_tag']
 
 
+stack_name = pulumi.get_stack()
+project_name = pulumi.get_project()
+pulumi_user = pulumi_config.get_pulumi_user()
+k8s_config = pulumi.Config('kubernetes')
+
+kic_image_build_project_name = project_name_from_project_dir('kic-image-build')
+kic_image_build_stack_ref_id = f"{pulumi_user}/{kic_image_build_project_name}/{stack_name}"
+kick_image_build_stack_ref = pulumi.StackReference(kic_image_build_stack_ref_id)
+ingress_image = kick_image_build_stack_ref.require_output('ingress_image')
+
 # We default to using the image name alias because it is a more precise definition
 # of the image type when we build from source.
 image_name = ingress_image.apply(select_image_name)
@@ -84,15 +57,30 @@ image_tag_alias = ingress_image.apply(select_image_tag_alias)
 image_id = ingress_image.apply(select_image_id)
 image_tag = ingress_image.apply(select_image_tag)
 
-repo_args = RepositoryPushArgs(repository_url=ecr_repository_url,
-                               credentials=ecr_credentials,
-                               image_id=image_id,
-                               image_name=image_name,
-                               image_tag=image_tag,
-                               image_tag_alias=image_tag_alias)
 
-# Push the images to the ECR repo
-ecr_repo_push = RepositoryPush(name='ingress-controller-repository-push',
-                               repository_args=repo_args)
+def push_to_container_registry(container_registry: ContainerRegistry) -> RepositoryPush:
+    if container_registry.login_to_registry():
+        repo_args = RepositoryPushArgs(repository_url=container_registry.registry_url,
+                                       image_id=image_id,
+                                       image_name=image_name,
+                                       image_tag=image_tag,
+                                       image_tag_alias=image_tag_alias)
 
-pulumi.export('ecr_repository', ecr_repo_push)
+        # Push the images to the container registry
+        _repo_push = RepositoryPush(name='ingress-controller-registry-push',
+                                    repository_args=repo_args,
+                                    check_if_id_matches_tag_func=container_registry.check_if_id_matches_tag)
+        return _repo_push
+    else:
+        raise 'Unable to log into container registry'
+
+
+# Dynamically determine the infrastructure provider and instantiate the
+# correlated class, then apply the pulumi async closures.
+infra_type = k8s_config.require('infra_type').lower()
+module = importlib.import_module(name=f'registries.{infra_type}')
+container_registry_class = module.CLASS
+repo_push: Output[RepositoryPush] = container_registry_class.instance(stack_name, pulumi_user)\
+    .apply(push_to_container_registry)
+
+pulumi.export('container_repo_push', Output.unsecret(repo_push))
---
+++ pulumi/python/utility/kic-image-push/registries/base_registry.py
@@ -0,0 +1,80 @@
+import base64
+import urllib
+from urllib import parse
+from typing import Optional, List
+
+import pulumi.log
+from pulumi import Input, Output
+import pulumi_docker as docker
+
+from kic_util import external_process
+
+
+class RegistryCredentials:
+    username: Input[str]
+    password: Input[str]
+
+    def __init__(self,
+                 username: Input[str],
+                 password: Input[str]):
+        self.username = username
+        self.password = password
+
+
+class ContainerRegistry:
+    stack_name: str
+    pulumi_user: str
+    credentials: Optional[RegistryCredentials]
+    registry_url: str
+
+    def __init__(self,
+                 stack_name: str,
+                 pulumi_user: str,
+                 registry_url: str,
+                 credentials: Optional[RegistryCredentials]) -> None:
+        super().__init__()
+        self.stack_name = stack_name
+        self.pulumi_user = pulumi_user
+        self.registry_url = registry_url
+        self.credentials = credentials
+
+    def format_registry_url_for_docker_login(self):
+        # We assume that the scheme is https because that's what is used most everywhere
+        registry_host_url = urllib.parse.urlparse(f'https://{self.registry_url}')
+        # We strip out the path from the URL because it isn't used when logging into a repository
+        return f'{registry_host_url.scheme}://{registry_host_url.hostname}'
+
+    def login_to_registry(self) -> Optional[docker.LoginResult]:
+        registry = docker.Registry(registry=self.format_registry_url_for_docker_login(),
+                                   username=self.credentials.username,
+                                   password=self.credentials.password)
+
+        docker.login_to_registry(registry=registry, log_resource=None)
+        pulumi.log.info(f'Logged into container registry: {registry.registry}')
+
+        if not docker.login_results:
+            return None
+        if docker.login_results[0]:
+            return docker.login_results[0]
+
+    def logout_of_registry(self):
+        docker_cmd = f'docker logout {self.format_registry_url_for_docker_login()}'
+        res, _ = external_process.run(cmd=docker_cmd)
+        pulumi.log.info(res)
+
+    def check_if_id_matches_tag(self, image_tag: str, new_image_id: str) -> bool:
+        return False
+
+    @classmethod
+    def instance(cls, stack_name: str, pulumi_user: str):
+        pass
+
+    @staticmethod
+    def decode_credentials(encoded_token: str) -> RegistryCredentials:
+        decoded = str(base64.b64decode(encoded_token), 'utf-8')
+        parts = decoded.split(':', 2)
+        if len(parts) != 2:
+            raise ValueError("Unexpected format for decoded ECR authorization token")
+        username = parts[0]
+        password = parts[1]
+        return RegistryCredentials(username=username, password=password)
