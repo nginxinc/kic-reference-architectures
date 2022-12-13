bc5850c2da6c2e24012d4d9f55e3c8a3770986c4
Jason Schmidt <j.schmidt@f5.com>
2022-12-13 10:59:02 -0700

    fix: set default registry option to basic, not starter

--- pulumi/python/infrastructure/digitalocean/container-registry/__main__.py
+++ pulumi/python/infrastructure/digitalocean/container-registry/__main__.py
@@ -9,7 +9,7 @@ config = pulumi.Config('docean')
 # valid values: starter, basic, professional
 subscription_tier = config.get('container_registry_subscription_tier')
 if not subscription_tier:
-    subscription_tier = 'starter'
+    subscription_tier = 'basic'
 region = config.get('region')
 if not region:
     region = 'sfo3'
